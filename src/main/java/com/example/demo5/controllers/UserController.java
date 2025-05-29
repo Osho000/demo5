f4a8fc244b3252e48841686b571e8c5f44dd2bb9
Svetlana Akhmetova <svetonia1@gmail.com>
2025-05-29 13:26:01 +0300

    Initial commit

---
+++ src/main/java/com/example/demo5/controllers/UserController.java
@@ -0,0 +1,14 @@
+package com.example.demo5.controllers;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/users")
+@RequiredArgsConstructor
+public class UserController {
+    public String getAboba() {
+        return "Aboba";
+    }
+}
